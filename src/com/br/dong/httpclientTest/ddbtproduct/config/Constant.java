2c52263df2b631d49fa642d54be55a4c7a32e392
apdoo <837484691@qq.com>
2014-11-21 22:52:11 +0800

    update 2014-11-21

--- src/com/br/dong/httpclientTest/ddbtproduct/config/Constant.java
+++ src/com/br/dong/httpclientTest/ddbtproduct/config/Constant.java
@@ -9,8 +9,18 @@ package com.br.dong.httpclientTest.ddbtproduct.config;
  * 常量配置类
  */
 public class Constant {
+    //--公用的配置参数
+    //html后缀
+    private static final String html=".html";
+    //需要替换的字符,这些字符windows文件名不能出现
+    private static final String COMMON_FILENAME_REG="(?:/|<|>|:|\\?|\\||\\*|\"|\\(|\\)|)";
+    //默认存盘路径目录
+    private static final  String COMMON_SAVE_PATH="D:\\蛋蛋bt\\download\\";
+    //
+    private final static int BUFFER = 1024;
     //---www.sis001task.com ip: 38.103.161.188的配置
     private static final String SIS001_USERNAME="ckwison";
     private static final String SIS001_PASSWORD="1234qwer!@#$";
-
+    //相对路径资源前加绝对路径
+    private static final String SIS001_ABSPRE="http://38.103.161.188/forum/";
 }
