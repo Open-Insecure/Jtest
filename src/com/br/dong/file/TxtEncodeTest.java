17bb3836e90c85b48b6479a7ef673d88de4b0b2e
apdoo <837484691@qq.com>
2014-12-06 17:13:36 +0800

    update 2014-11-07

---
+++ src/com/br/dong/file/TxtEncodeTest.java
@@ -0,0 +1,13 @@
+package com.br.dong.file;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dong
+ * Date: 14-12-1
+ * Time: 下午9:38
+ * To change this template use File | Settings | File Templates.
+ * 测试不同txt的编码文件的问题
+ */
+public class TxtEncodeTest {
+
+}
